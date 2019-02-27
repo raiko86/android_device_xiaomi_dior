8e92ca1fb141fd02a0c55804b42e41fae146d8c8
Raiko Horn <raiko.horn@gmail.com>
2019-02-27 12:50:37 +0200

    :)

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,4 +1,9 @@
 /*
+ *
+ *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ *  Not a Contribution, Apache license notifications and license are retained
+ *  for attribution purposes only.
+ *
  * Copyright (C) 2012 The Android Open Source Project
  * Copyright (C) 2016 The CyanogenMod Project
  *
